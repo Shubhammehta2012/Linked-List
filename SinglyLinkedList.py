808c9ecbf85c2dc68a48c554a349257dfdd6572a
Shubham Mehta <31146862+Shubhammehta2012@users.noreply.github.com>
2018-08-21 02:06:52 -0700

    Update SinglyLinkedList.py

--- SinglyLinkedList.py
+++ SinglyLinkedList.py
@@ -1,9 +1,5 @@
-# Author: OMKAR PATHAK
-
-# Linked List and Node can be accomodated in separate classes for convenience
 
 class Node(object):
-    # Each node has its data and a pointer that points to next node in the Linked List
     def __init__(self, data, next = None):
         self.data = data;
         self.next = next;
@@ -16,27 +12,22 @@ class Node(object):
     def getData(self):
         return self.data
 
-    # function to set next node
     def setNext(self, next):
         self.next = next
 
-    # function to get the next node
     def getNext(self):
         return self.next
 
 class LinkedList(object):
-    # Defining the head of the linked list
     def __init__(self):
         self.head = None
 
-    # printing the data in the linked list
     def printLinkedList(self):
         temp = self.head
         while(temp):
             print(temp.data, end=' ')
             temp = temp.next
 
-    # inserting the node at the beginning
     def insertAtStart(self, data):
         if self.head == None:
             newNode = Node(data)
@@ -46,7 +37,6 @@ class LinkedList(object):
             newNode.next = self.head
             self.head = newNode
 
-    # inserting the node in between the linked list (after a specific node)
     def insertBetween(self, previousNode, data):
         if (previousNode.next is None):
             print('Previous node should have next node!')
@@ -55,39 +45,33 @@ class LinkedList(object):
             newNode.next = previousNode.next
             previousNode.next = newNode
 
-    # inserting at the end of linked list
     def insertAtEnd(self, data):
         newNode = Node(data)
         temp = self.head
-        while(temp.next != None):         # get last node
+        while(temp.next != None):
             temp = temp.next
         temp.next = newNode
 
-    # deleting an item based on data(or key)
     def delete(self, data):
         temp = self.head
-        # if data/key is found in head node itself
         if (temp.next is not None):
             if(temp.data == data):
                 self.head = temp.next
                 temp = None
                 return
             else:
-                #  else search all the nodes
                 while(temp.next != None):
                     if(temp.data == data):
                         break
-                    prev = temp          #save current node as previous so that we can go on to next node
+                    prev = temp          
                     temp = temp.next
 
-                # node not found
                 if temp == None:
                     return
 
                 prev.next = temp.next
                 return
 
-    # iterative search
     def search(self, node, data):
         if node == None:
             return False
@@ -97,13 +81,13 @@ class LinkedList(object):
 
 if __name__ == '__main__':
     List = LinkedList()
-    List.head = Node(1)                # create the head node
+    List.head = Node(1)               
     node2 = Node(2)
-    List.head.setNext(node2)           # head node's next --> node2
+    List.head.setNext(node2)    
     node3 = Node(3)
-    node2.setNext(node3)               # node2's next --> node3
-    List.insertAtStart(4)              # node4's next --> head-node --> node2 --> node3
-    List.insertBetween(node2, 5)       # node2's next --> node5
+    node2.setNext(node3)             
+    List.insertAtStart(4)        
+    List.insertBetween(node2, 5)   
     List.insertAtEnd(6)
     List.printLinkedList()
     print()
